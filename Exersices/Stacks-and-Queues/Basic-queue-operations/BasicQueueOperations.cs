13701bde32a2c4d5ba54d57b50b62413916e56d9
AleksandarSt <A_V_S_@abv.bg>
2016-05-26 02:02:32 +0300

    stack and queue update

---
+++ Exersices/Stacks-and-Queues/Basic-queue-operations/BasicQueueOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_queue_operations
+{
+    public class BasicQueueOperations
+    {
+        public static void Main()
+        {
+            int[] parameters = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] numbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue=new Queue<int>();
+
+            foreach (var number in numbers)
+            {
+                queue.Enqueue(number);
+            }
+
+            for (int i = 0; i < parameters[1]; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                string result = queue.Contains(parameters[2])
+                    ? "true"
+                    : queue.ToArray().Min().ToString();
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Exersices/Stacks-and-Queues/Calculate-sequence-with-queue/CalculateSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Calculate_sequence_with_queue
+{
+    public class CalculateSequence
+    {
+        public static void Main()
+        {
+            const int lastMemberOfSequence = 50;
+
+            BigInteger currentSum = BigInteger.Parse(Console.ReadLine());
+            int elementNumber = 0;
+            Queue<BigInteger> queue = new Queue<BigInteger>();
+
+            queue.Enqueue(currentSum);
+
+            do
+            {
+                currentSum = queue.Dequeue();
+                Console.Write("{0} ", currentSum);
+                queue.Enqueue(currentSum + 1);
+                queue.Enqueue(2*currentSum + 1);
+                queue.Enqueue(currentSum + 2);
+                elementNumber++;
+
+            } while (elementNumber < lastMemberOfSequence);
+        }
+    }
+}
