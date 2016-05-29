ad7cd4c42667b85b54239d796dc7533d71f6d811
AleksandarSt <A_V_S_@abv.bg>
2016-05-30 01:47:38 +0300

    simple text editor, stack fibonachi

---
+++ Exersices/Stacks-and-Queues/Simple-Text-Editor/SimpleText-Editor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_Text_Editor
+{
+    public class SimpleTextEditor
+    {
+        static void Main()
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+
+            Stack<string> stack=new Stack<string>();
+            stack.Push("");
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] currentCommand = Console.ReadLine().Split();
+                StringBuilder text = new StringBuilder();
+
+                switch (currentCommand[0])
+                {
+                    case "1":
+                        text.Append(stack.Peek());
+                        text.Append(currentCommand[1]);
+                        stack.Push(text.ToString());
+                        break;
+                    case "2":
+                        text.Append(stack.Peek());
+                        text.Remove(text.Length - int.Parse(currentCommand[1]), int.Parse(currentCommand[1]));
+                        stack.Push(text.ToString());
+                        break;
+                    case "3":
+                        text.Append(stack.Peek());
+                        Console.WriteLine(text[int.Parse(currentCommand[1]) - 1]);
+                        break;
+                    case "4":
+                        stack.Pop();
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ Exersices/Stacks-and-Queues/Stack-Fibonacci/StackFibonacci .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_Fibonacci
+{
+    class StackFibonacci
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Stack<long> stack=new Stack<long>();
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i==0||i==1)
+                {
+                    stack.Push(1);
+                }
+                else
+                {
+                    long lastElement = stack.Pop();
+                    long newElement = lastElement + stack.Peek();
+                    stack.Push(lastElement);
+                    stack.Push(newElement);
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
