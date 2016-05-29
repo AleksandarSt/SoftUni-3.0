6aa82688e72fb127f590542ae463afa6d8dd9e06
AleksandarSt <A_V_S_@abv.bg>
2016-05-30 00:33:15 +0300

    recursive fibonachi added

---
+++ Exersices/Stacks-and-Queues/Recursive-Fibonachi/RecursiveFibonachi.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Recursive_Fibonachi
+{
+    class RecursiveFibonachi
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Queue<long> queue=new Queue<long>();
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i==0||i==1)
+                {
+                    queue.Enqueue(1);
+                }
+                else
+                {
+                    queue.Enqueue(queue.Dequeue()+queue.Peek());
+                }
+            }
+
+            queue.Dequeue();
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
