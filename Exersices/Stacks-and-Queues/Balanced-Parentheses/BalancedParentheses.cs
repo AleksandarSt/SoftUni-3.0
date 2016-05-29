19bb4731b7927d1a3fa4ca5f1f675de6e1661ba3
AleksandarSt <A_V_S_@abv.bg>
2016-05-29 20:26:50 +0300

    balanced paranthesis

---
+++ Exersices/Stacks-and-Queues/Balanced-Parentheses/BalancedParentheses.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balanced_Parentheses
+{
+    class BalancedParentheses
+    {
+        static void Main()
+        {
+           char[] Opening = { '{', '[', '(' };
+
+            string input = Console.ReadLine();
+
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Opening.Contains(input[i]))
+                {
+                    stack.Push(input[i].ToString());
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+
+                    if (!IsBalanced(stack.Peek(), input[i].ToString()))
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+
+                    stack.Pop();
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+
+        private static bool IsBalanced(string openingParenthesis, string closingParanthesis)
+        {
+            bool isBalanced;
+
+            if (openingParenthesis=="(")
+            {
+                isBalanced = closingParanthesis == ")";
+            }
+            else if (openingParenthesis=="[")
+            {
+                isBalanced = closingParanthesis == "]";
+            }
+            else
+            {
+                isBalanced = closingParanthesis == "}";
+            }
+
+            return isBalanced;
+        }
+    }
+}
