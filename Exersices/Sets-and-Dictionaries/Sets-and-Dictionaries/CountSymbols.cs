44eefd4e66f2d8fb69c83512edeefc3f4be46884
AleksandarSt <A_V_S_@abv.bg>
2016-05-31 00:51:38 +0300

    count synbols added

---
+++ Exersices/Sets-and-Dictionaries/Sets-and-Dictionaries/CountSymbols.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Symbols
+{
+    public class CountSymbols
+    {
+        static void Main()
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+
+            Dictionary<char, int> dictionary=new Dictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!dictionary.ContainsKey(text[i]))
+                {
+                    dictionary.Add(text[i],1);
+                }
+                else
+                {
+                    dictionary[text[i]] += 1;
+                }
+            }
+
+            foreach (var item in dictionary.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine("{0}: {1} time/s",item.Key,item.Value);
+            }
+
+            
+        }
+    }
+}
