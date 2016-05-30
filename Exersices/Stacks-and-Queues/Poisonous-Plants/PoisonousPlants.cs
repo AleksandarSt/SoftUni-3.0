d0c2a05a2eb25071bd6465be4b7fcca7f2337c3b
AleksandarSt <A_V_S_@abv.bg>
2016-05-31 00:24:55 +0300

    poisonous plants reading input added

---
+++ Exersices/Stacks-and-Queues/Poisonous-Plants/PoisonousPlants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Poisonous_Plants
+{
+    public class PoisonousPlants
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] plants = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+        }
+    }
+}
