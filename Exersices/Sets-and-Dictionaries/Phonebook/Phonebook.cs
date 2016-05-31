9b0f7e0a1a14fe2578194e6f351f7576f2740604
AleksandarSt <A_V_S_@abv.bg>
2016-06-01 01:25:21 +0300

    Phonebook added

---
+++ Exersices/Sets-and-Dictionaries/Phonebook/Phonebook.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phonebook
+{
+    class Phonebook
+    {
+        static void Main()
+        {
+            Dictionary<string, string> phonebook=new Dictionary<string, string>();
+
+            do
+            {
+                string line = Console.ReadLine();
+                if (line=="search")
+                {
+                    break;
+                }
+
+                string currentKey=line.Split(new char[]{'-'},StringSplitOptions.RemoveEmptyEntries)[0];
+                string currentValue = line.Split(new char[] {'-'}, StringSplitOptions.RemoveEmptyEntries)[1];
+
+                if (!phonebook.ContainsKey(currentKey))
+                {
+                    phonebook.Add(currentKey,currentValue);
+                }
+                else
+                {
+                    phonebook[currentKey] = currentValue;
+                }
+            } while (true);
+
+            do
+            {
+                string name = Console.ReadLine();
+
+                if (name=="stop")
+                {
+                    break;
+                }
+
+                if (!phonebook.ContainsKey(name))
+                {
+                    Console.WriteLine("Contact {0} does not exist.",name);
+                }
+                else
+                {
+                    Console.WriteLine("{0} -> {1}", name, phonebook[name]);
+                }
+
+            } while (true);
+        }
+    }
+}
