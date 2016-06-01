acfc534b3f6cd3f1de99f216a361e841e2428af3
AleksandarSt <A_V_S_@abv.bg>
2016-06-02 01:08:37 +0300

    miners task and email fixer added

---
+++ Exersices/Sets-and-Dictionaries/A-miner-task/MinerTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace A_miner_task
+{
+    class MinerTask
+    {
+        static void Main()
+        {
+            Dictionary<string, BigInteger> miner = new Dictionary<string, BigInteger>();
+            string line = Console.ReadLine();
+            int counter = 1;
+            string currentKey="";
+
+            do
+            {
+                if (line=="stop")
+                {
+                    return;
+                }
+
+                if (counter%2 != 0)
+                {
+                    currentKey = line;
+                    if (!miner.ContainsKey(line))
+                    {
+                        miner.Add(line, 0);
+                    }
+                }
+                else
+                {
+                    miner[currentKey] += BigInteger.Parse(line);
+                }
+
+                line = Console.ReadLine();
+                counter++;
+            } while (line != "stop");
+
+            foreach (var resource in miner)
+            {
+                Console.WriteLine("{0} -> {1}", resource.Key, resource.Value);
+            }
+                
+
+        }
+    }
+}
---
+++ Exersices/Sets-and-Dictionaries/Fix-emails/FixEmails.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fix_emails
+{
+    class FixEmails
+    {
+        static void Main()
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            string name = Console.ReadLine();
+            string email;
+
+            do
+            {
+
+
+                if (name == "stop")
+                {
+                    break;
+                }
+
+                email = Console.ReadLine();
+                string lastSymbols = email.Substring(email.Length - 3, 3);
+
+                if (lastSymbols != ".us" && lastSymbols != ".uk")
+                {
+                    if (!dictionary.ContainsKey(name))
+                    {
+                        dictionary.Add(name, email);
+                    }
+                    else
+                    {
+                        dictionary[name] = email;
+                    }
+                }
+
+                name = Console.ReadLine();
+            } while (name != "stop" || email != "stop");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0} -> {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
