12f0cef629f002317e2989a0e8961ff5676786b1
AleksandarSt <A_V_S_@abv.bg>
2016-06-15 23:58:29 +0300

    push
    push

---
+++ Exersices/Regular-expressions/Regular-expressions/MatchFullName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regular_expressions
+{
+    public class MatchFullName
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\b(?<firstname>[A-Z]{1}[a-z]+) (?<lastname>[A-Z]{1}[a-z]+)\b";
+
+            Regex regex=new Regex(pattern);
+            while (true)
+            {
+                if (regex.IsMatch(text))
+                {
+                    Console.WriteLine(regex.Match(text));
+                }
+                
+                text = Console.ReadLine();
+            }
+            
+        }
+    }
+}
