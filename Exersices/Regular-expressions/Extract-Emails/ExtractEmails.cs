82405e7ffdcc9033fa98529d9bb48ef2cd444d55
AleksandarSt <A_V_S_@abv.bg>
2016-06-18 18:49:15 +0300

    Extract Emails

---
+++ Exersices/Regular-expressions/Extract-Emails/ExtractEmails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Extract_Emails
+{
+    public class ExtractEmails
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?:^|\s)((?:[a-zA-Z0-9]+[.\-_])*[a-zA-Z0-9]+@(?:[a-zA-Z]+-?)+(?:\.[a-zA-Z]+)+)(?:\.\s)?";
+            Regex matcher=new Regex(pattern);
+            MatchCollection collection = matcher.Matches(input);
+
+            for (int i = 0; i < collection.Count; i++)
+            {
+                Console.WriteLine(collection[i].Groups[1].Value);
+            }
+        }
+    }
+}
