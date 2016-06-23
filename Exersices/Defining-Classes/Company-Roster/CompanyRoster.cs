54bcda892cfde0b2621999f674890eb778f5766d
AleksandarSt <A_V_S_@abv.bg>
2016-06-24 01:20:52 +0300

    Company roster 40 points

---
+++ Exersices/Defining-Classes/Company-Roster/CompanyRoster.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Company_Roster
+{
+    class CompanyRoster
+    {
+        static void Main()
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            List<Employee> employees=new List<Employee>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                var employeeInfo = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                var employee=new Employee(
+                    employeeInfo[0],
+                    decimal.Parse(employeeInfo[1]),
+                    employeeInfo[2],
+                    employeeInfo[3]);
+
+                if (employeeInfo.Length == 5)
+                {
+                    var emailOrAge = employeeInfo[4];
+
+                    if (emailOrAge.Contains("@"))
+                    {
+                        employee.email = emailOrAge;
+                        employee.age = -1;
+                    }
+                    else
+                    {
+                        employee.email = "n/a";
+                        employee.age = int.Parse(emailOrAge);
+                    }
+
+
+                }
+                else if(employeeInfo.Length == 6)
+                {
+                    employee.email = employeeInfo[4];
+                    employee.age = int.Parse(employeeInfo[5]);
+                }
+
+                employees.Add(employee);
+            }
+
+            var result = employees
+                .GroupBy(e => e.deparment)
+                .Select
+                (
+                    e =>
+                        new
+                        {
+                            Departmen = e.Key,
+                            Salary = e.Average(emp => emp.salary),
+                            Employees=e.OrderByDescending(emp=>emp.salary)
+                        }
+                )
+                .FirstOrDefault();
+
+            Console.WriteLine($"Highest Average Salary: {result.Departmen}");
+
+            foreach (var employee in result
+                .Employees)
+            {
+                Console.WriteLine($"{employee.name} {employee.salary:f2} {employee.email} {employee.age}");
+            }
+        }
+    }
+}
---
+++ Exersices/Defining-Classes/Company-Roster/Employee.cs
@@ -0,0 +1,32 @@
+﻿namespace Company_Roster
+{
+    public class Employee
+    {
+        public string name;
+        public decimal salary;
+        public string position;
+        public string deparment;
+        public string email;
+        public int age;
+
+        public Employee(string name, decimal salary, string position, string department)
+        {
+            this.name = name;
+            this.salary = salary;
+            this.position = position;
+            this.deparment = department;
+        }
+
+        public Employee(string email, string name, decimal salary, string position, string department)
+            : this(name, salary, position, department)
+        {
+            this.email = email;
+        }
+
+        public Employee(int age, string email, string name, decimal salary, string position, string department)
+            : this(email, name, salary, position, department)
+        {
+            this.age = age;
+        }
+    }
+}
