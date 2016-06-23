46ff2a9edd96eaf7b54bf6d59816ad9fb427b3fe
AleksandarSt <A_V_S_@abv.bg>
2016-06-23 16:29:04 +0300

    Define class, create constructor
    Define class, create constructor

---
+++ Exersices/Defining-Classes/Person/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace Person
+{
+	public class Person
+	{
+		public string name;
+		public int age;
+
+		public Person()
+		{
+			this.name = "No name";
+			this.age = 1;
+		}
+
+		public Person(int age)
+			:this()
+		{
+			this.age = age;
+		}
+
+		public Person(int age, string name)
+
+		{
+			this.age = age;
+			this.name = name;
+		}
+	}
+}
