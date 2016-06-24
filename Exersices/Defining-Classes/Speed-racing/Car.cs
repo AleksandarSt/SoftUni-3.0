d2c2e6fc20faac0432a7ee489e65aefe8063e1e7
AleksandarSt <A_V_S_@abv.bg>
2016-06-24 23:30:02 +0300

    Speed racing added

---
+++ Exersices/Defining-Classes/Speed-racing/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Speed_racing
+{
+    public class Car
+    {
+        public string model;
+        public decimal fuelAmount;
+        public decimal fuelCostPerKm;
+        public decimal distanceTraveled;
+
+        public Car(string model, decimal fuelAmount, decimal fuelCostPerKm)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelCostPerKm = fuelCostPerKm;
+        }
+
+        public void Drive(string carModel, decimal amountOfKm)
+        {
+            if (this.model==carModel&&amountOfKm*fuelCostPerKm<=fuelAmount)
+            {
+                this.fuelAmount = fuelAmount - amountOfKm*fuelCostPerKm;
+                this.distanceTraveled += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ Exersices/Defining-Classes/Speed-racing/SpeedRacing.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Speed_racing
+{
+    class SpeedRacing
+    {
+        static void Main()
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            var cars=new List<Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                var currentCar = new Car(carInfo[0], decimal.Parse(carInfo[1]), decimal.Parse(carInfo[2]));
+                
+                cars.Add(currentCar);
+            }
+
+            string line = Console.ReadLine();
+
+            while (line!="End")
+            {
+                string[] currentCommand = line.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var car in cars)
+                {
+                    if (car.model==currentCommand[1])
+                    {
+                        car.Drive(currentCommand[1],decimal.Parse(currentCommand[2]));
+                    }
+                }
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.model} {car.fuelAmount:F2} {car.distanceTraveled}");
+            }
+        }
+    }
+}
