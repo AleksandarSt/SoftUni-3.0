75436e3c2eaf3b7ded56c257bf8de09a51eac9cc
AleksandarSt <A_V_S_@abv.bg>
2016-07-02 21:03:37 +0300

    person
    Person for 33 points

---
+++ Exersices/Inheritance/Person/Models/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace Person.Models
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        protected string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)||value.Length<3)
+                {
+                    throw new ArgumentException($"Name’s length should not be less than 3 symbols!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        protected virtual int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                {
+                    throw new ArgumentException("Age cannot be null!");
+                }
+                if (value < 0)
+                {
+                 throw   new ArgumentException("Age must be positive!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
