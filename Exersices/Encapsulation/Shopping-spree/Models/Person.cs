8f425de5267b2abaf7cd35ca1ac3bfedd42bb3e2
AleksandarSt <A_V_S_@abv.bg>
2016-07-02 18:30:21 +0300

    Encapsulation animal farm and shopping spree added

---
+++ Exersices/Encapsulation/Shopping-spree/Models/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shopping_spree.Models
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.Basket=new List<Product>();
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return this.money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public List<Product> Basket { get; set; }
+
+        public void BuyProduct(Product product)
+        {
+            if (product.Money>this.Money)
+            {
+                throw new InvalidOperationException($"{this.Name} can't afford {product.Name}");
+            }
+                this.Money -= product.Money;
+                this.Basket.Add(product);
+        }
+
+        public override string ToString()
+        {
+            string name = this.Name;
+
+            string products = string.Join(", ", this.Basket);
+
+            if (string.IsNullOrEmpty(products))
+            {
+                products = "Nothing bought";
+            }
+
+            string result = $"{name} - {products}";
+
+            return result;
+        }
+    }
+}
---
+++ Exersices/Encapsulation/Shopping-spree/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.SqlServer.Server;
+using Shopping_spree.Models;
+
+namespace Shopping_spree
+{
+    class Startup
+    {
+        static void Main()
+        {
+            try
+            {
+                var persons = new Dictionary<string, Person>();
+                var products = new Dictionary<string, Product>();
+
+                string[] personsInfo = Console.ReadLine()
+                    .Split(new[] {';', '='}, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < personsInfo.Length; i += 2)
+                {
+                    var currentPerson = new Person(personsInfo[i], decimal.Parse(personsInfo[i + 1]));
+
+                    persons.Add(personsInfo[i], currentPerson);
+                }
+
+                string[] productsInfo = Console.ReadLine()
+                    .Split(new[] {';', '='}, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < productsInfo.Length; i += 2)
+                {
+                    var currentProduct = new Product(productsInfo[i], decimal.Parse(productsInfo[i + 1]));
+
+                    products.Add(productsInfo[i], currentProduct);
+                }
+
+                string command = Console.ReadLine();
+
+                while (command != "END")
+                {
+                    string[] commandArgs = command.Split();
+                    var personName = commandArgs[0];
+                    var productName = commandArgs[1];
+
+                    Person person = persons[personName];
+                    Product product = products[productName];
+
+                    try
+                    {
+                        person.BuyProduct(product);
+                        Console.WriteLine($"{person.Name} bought {product.Name}");
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    command = Console.ReadLine();
+                }
+
+                foreach (Person person in persons.Values)
+                {
+                    Console.WriteLine(person);
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
