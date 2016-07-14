9941c9f56e61a727c054b0806da825240cd9acee
AleksandarSt <A_V_S_@abv.bg>
2016-07-14 13:41:52 +0300

    Telephony
    Interfaces and abstraction

---
+++ Exersices/Interfaces-and-Abstraction/Telephony/Smartphone.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Telephony
+{
+	public class Smartphone : ICallable, IBrowsable
+	{
+		public void CallNumber(string number)
+		{
+			if (CanCallNumber(number))
+			{
+				Console.WriteLine($"Calling... {number}");
+			}
+			else
+			{
+				Console.WriteLine("Invalid number!");
+			}
+		}
+
+		public void BrowseWebSite(string url)
+		{
+			if (CanBrowseWebSite(url))
+			{
+				Console.WriteLine($"Browsing: {url}!");
+			}
+			else
+			{
+				Console.WriteLine("Invalid URL!");
+			}
+		}
+
+		private bool CanCallNumber(string number)
+		{
+			string pattern = @"^[0-9]*$";
+			Regex regex = new Regex(pattern);
+
+			return regex.IsMatch(number);
+		}
+
+		private bool CanBrowseWebSite(string url)
+		{
+			string pattern = @"^\D*$";
+			Regex regex = new Regex(pattern);
+
+			return regex.IsMatch(url);
+		}
+	}
+}
---
+++ Exersices/Interfaces-and-Abstraction/Telephony/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Telephony
+{
+	class Startup
+	{
+		static void Main()
+		{
+			string[] numbersToCall = Console.ReadLine().Split();
+			string[] websitesToCall = Console.ReadLine().Split();
+
+			var smarthphone=new Smartphone();
+
+			foreach (var number in numbersToCall)
+			{
+				smarthphone.CallNumber(number);
+			}
+
+			foreach (var url in websitesToCall)
+			{
+				smarthphone.BrowseWebSite(url);
+			}
+		}
+	}
+}
