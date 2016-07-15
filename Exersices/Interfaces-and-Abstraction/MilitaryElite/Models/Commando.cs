c44cbe9cb56ff7432999e40c4b970a74b6225b65
AleksandarSt <A_V_S_@abv.bg>
2016-07-15 17:00:10 +0300

    Military elite
    complete command

--- Exersices/Interfaces-and-Abstraction/MilitaryElite/Models/Commando.cs
+++ Exersices/Interfaces-and-Abstraction/MilitaryElite/Models/Commando.cs
@@ -4,6 +4,8 @@ namespace MilitaryElite.Models
 {
 	public class Commando : SpecialisedSoldier
 	{
+		private List<Mission> missions;
+
 		public Commando(int id, string firstName, string lastName, double salary, CorpTypes corpType,List<Mission> missions) :
 			base(id, firstName, lastName, salary, corpType)
 		{
@@ -11,5 +13,10 @@ namespace MilitaryElite.Models
 		}
 
 		public List<Mission> Missions { get; set; }
+
+		public void CompleteMission(Mission mission)
+		{
+			mission.State=MissionStates.Finished;
+		}
 	}
 }
--- Exersices/Interfaces-and-Abstraction/MilitaryElite/Models/Mission.cs
+++ Exersices/Interfaces-and-Abstraction/MilitaryElite/Models/Mission.cs
@@ -6,13 +6,22 @@
 		private string codeName;
 		private MissionStates state;
 
+		public Mission(string codeName, MissionStates state)
+		{
+			this.CodeName = codeName;
+			this.State = state;
+		}
+
 		public string CodeName
 		{
 			get { return this.codeName; }
 			set { this.codeName = value; }
 		}
 
-		
-
+		public MissionStates State
+		{
+			get { return this.state; }
+			set { this.state = value; }
+		}
 	}
 }
