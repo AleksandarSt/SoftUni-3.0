d9aa0e399c9546845b7390aaf9fabc6d96e34ad9
AleksandarSt <A_V_S_@abv.bg>
2016-07-20 01:19:06 +0300

    Box class added
    Box class added

---
+++ Exersices/Generics/GenericBox/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericBox
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Box<int> test=new Box<int>(123123);
+
+            Console.WriteLine(test);
+        }
+    }
+}
