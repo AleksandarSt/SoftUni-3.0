c2cfbe0f96d8c221c47f4d6a13bd93a5c1c92e12
AleksandarSt <A_V_S_@abv.bg>
2016-12-04 11:37:21 +0200

    bookshopsystem

---
+++ Exersices/BookShopSystem/BookShopSystem.Models/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSystem.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Range(0,50),Required]
+        public string Title { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public EditionTypes EditionType { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Copies { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+
+    public enum EditionTypes
+    {
+        Normal,
+        Promo,
+        Gold
+    }
+}
